c4c3aadd871bc0023266e4a221a2dd220c11c501
frosinacvetkovska <frosinacvetkovska29@gmail.com>
2024-01-27 19:00:07 +0100

    Migration of database and fixing bugs on backend side

--- djangodemoapp/demoapp/urls.py
+++ djangodemoapp/demoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import predict, demoapp
 
 urlpatterns = [
+    path('predict/', predict, name='predict'),
     path('demoapp/', views.demoapp, name='demoapp'),
 ]
--- djangodemoapp/demoapp/views.py
+++ djangodemoapp/demoapp/views.py
@@ -8,15 +8,18 @@ from django.template import loader
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
 
-from demoapp.forms import PredictionForm
-from demoapp.models import Prediction
+# from demoapp.forms import PredictionForm
+# from demoapp.models import Prediction
+from .forms import PredictionForm
+from .models import Prediction
+
 
 import os
 import cv2
 
-def demoapp(request):
-    template = loader.get_template('frontpage.html')
-    return HttpResponse(template.render())
+# def demoapp(request):
+#     template = loader.get_template('prediction.html')
+#     return HttpResponse(template.render())
 
 def predict_image(model_number, path, size_x: int = 224, size_y: int = 224):
     print(os. getcwd())
@@ -24,8 +27,42 @@ def predict_image(model_number, path, size_x: int = 224, size_y: int = 224):
     print(img)
     img = cv2.resize(img, (size_x, size_y))
     img = np.expand_dims(img, axis=0)
-    model = keras.models.load_model(f'classification/prediction/models/model{model_number}.h5')
+    model = keras.models.load_model(f'demoapp/models/model{model_number}.h5')
     return model.predict(img, verbose=False)[0]
+
+# def predict(request: HttpRequest):
+#     context = dict()
+#     context["form"] = PredictionForm
+
+#     if request.method == "POST":
+#         form_data = PredictionForm(data=request.POST, files=request.FILES)
+#         if form_data.is_valid():
+#             prediction: Prediction = form_data.save(commit=False)
+#             prediction.image = form_data.cleaned_data["image"]
+#             prediction.likelihood_indoor= 0
+#             prediction.likelihood_outdoor =0
+#             prediction.predicted_value = 'none'
+#             prediction.save()
+
+#             likelihoods = predict_image(prediction.model, prediction.image.url)
+#             greater_likelikood = likelihoods[0]
+#             if prediction.model!='2':
+#                 prediction.likelihood_indoor= likelihoods[0]
+#                 prediction.likelihood_outdoor = likelihoods[1]
+#                 if likelihoods[0]>likelihoods[1]:
+#                     prediction.predicted_value = 'indoor'
+#                 else:
+#                     prediction.predicted_value = 'outdoor'
+#                     greater_likelikood = likelihoods[1]
+#             else:
+#                 if likelihoods[0]<=0.5:
+#                     prediction.predicted_value = 'indoor'
+#                     greater_likelikood = 1-likelihoods[0]
+#                 else:
+#                     prediction.predicted_value = 'outdoor'
+#             prediction.save()
+#             return redirect(f"?image={prediction.image.url}&class={prediction.predicted_value}&likelihood={greater_likelikood}&model={prediction.model}")
+#     return render(request, "prediction.html", context=context)
 
 def predict(request: HttpRequest):
     context = dict()
@@ -36,27 +73,22 @@ def predict(request: HttpRequest):
         if form_data.is_valid():
             prediction: Prediction = form_data.save(commit=False)
             prediction.image = form_data.cleaned_data["image"]
-            prediction.likelihood_indoor= 0
-            prediction.likelihood_outdoor =0
+            prediction.likelihood_indoor = 0
+            prediction.likelihood_outdoor = 0
             prediction.predicted_value = 'none'
             prediction.save()
 
-            likelihoods = predict_image(prediction.model, prediction.image.url)
-            greater_likelikood = likelihoods[0]
-            if prediction.model!='2':
-                prediction.likelihood_indoor= likelihoods[0]
-                prediction.likelihood_outdoor = likelihoods[1]
-                if likelihoods[0]>likelihoods[1]:
-                    prediction.predicted_value = 'indoor'
-                else:
-                    prediction.predicted_value = 'outdoor'
-                    greater_likelikood = likelihoods[1]
+            probabilities = predict_image(prediction.model, prediction.image.url)
+
+            prediction.likelihood_indoor = probabilities[0]
+            prediction.likelihood_outdoor = 1 - probabilities[0]  # Assuming binary classification
+
+            if probabilities[0] >= 0.5:
+                prediction.predicted_value = 'outdoor'
             else:
-                if likelihoods[0]<=0.5:
-                    prediction.predicted_value = 'indoor'
-                    greater_likelikood = 1-likelihoods[0]
-                else:
-                    prediction.predicted_value = 'outdoor'
+                prediction.predicted_value = 'indoor'
+
             prediction.save()
-            return redirect(f"?image={prediction.image.url}&class={prediction.predicted_value}&likelihood={greater_likelikood}&model={prediction.model}")
-    return render(request, "frontpage.html", context=context)
+            return redirect(f"?image={prediction.image.url}&class={prediction.predicted_value}&likelihood={probabilities[0]}&model={prediction.model}")
+    
+    return render(request, "prediction.html", context=context)
