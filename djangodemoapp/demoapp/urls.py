300e951f13110b5488e3fa7cccdc0891d52e0217
frosinacvetkovska <frosinacvetkovska29@gmail.com>
2024-01-28 16:32:42 +0100

    Fixed urls.py file and deleted comments

--- djangodemoapp/demoapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import predict, demoapp
-
-urlpatterns = [
-    path('predict/', predict, name='predict'),
-    path('demoapp/', views.demoapp, name='demoapp'),
-]
