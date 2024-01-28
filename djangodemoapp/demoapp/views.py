5b50fb31bd449b91c4ddd4dffe380236170f9102
frosinacvetkovska <frosinacvetkovska29@gmail.com>
2024-01-28 16:33:02 +0100

    Fixed urls.py file and deleted comments

--- djangodemoapp/demoapp/views.py
+++ djangodemoapp/demoapp/views.py
@@ -17,9 +17,6 @@ from .models import Prediction
 import os
 import cv2
 
-# def demoapp(request):
-#     template = loader.get_template('prediction.html')
-#     return HttpResponse(template.render())
 
 def predict_image(model_number, path, size_x: int = 224, size_y: int = 224):
     print(os. getcwd())
@@ -30,39 +27,6 @@ def predict_image(model_number, path, size_x: int = 224, size_y: int = 224):
     model = keras.models.load_model(f'demoapp/models/model{model_number}.h5')
     return model.predict(img, verbose=False)[0]
 
-# def predict(request: HttpRequest):
-#     context = dict()
-#     context["form"] = PredictionForm
-
-#     if request.method == "POST":
-#         form_data = PredictionForm(data=request.POST, files=request.FILES)
-#         if form_data.is_valid():
-#             prediction: Prediction = form_data.save(commit=False)
-#             prediction.image = form_data.cleaned_data["image"]
-#             prediction.likelihood_indoor= 0
-#             prediction.likelihood_outdoor =0
-#             prediction.predicted_value = 'none'
-#             prediction.save()
-
-#             likelihoods = predict_image(prediction.model, prediction.image.url)
-#             greater_likelikood = likelihoods[0]
-#             if prediction.model!='2':
-#                 prediction.likelihood_indoor= likelihoods[0]
-#                 prediction.likelihood_outdoor = likelihoods[1]
-#                 if likelihoods[0]>likelihoods[1]:
-#                     prediction.predicted_value = 'indoor'
-#                 else:
-#                     prediction.predicted_value = 'outdoor'
-#                     greater_likelikood = likelihoods[1]
-#             else:
-#                 if likelihoods[0]<=0.5:
-#                     prediction.predicted_value = 'indoor'
-#                     greater_likelikood = 1-likelihoods[0]
-#                 else:
-#                     prediction.predicted_value = 'outdoor'
-#             prediction.save()
-#             return redirect(f"?image={prediction.image.url}&class={prediction.predicted_value}&likelihood={greater_likelikood}&model={prediction.model}")
-#     return render(request, "prediction.html", context=context)
 
 def predict(request: HttpRequest):
     context = dict()
